4b322815bcf9ea5e43dfca1348e5205c9e4df550
Jenek Fedorow <efedorov@bivgroup.ru>
2023-06-20 15:10:02 +0300

    added scss loader

--- src/index.ts
+++
@@ -1,5 +0,0 @@
-import {someFn} from "./test";
-
-console.log(someFn(123));
-
-document.body.innerHTML = `<div>Hello WORLD</div>`
--- src/test.ts
+++
@@ -1,6 +0,0 @@
-export function someFn(arg: number): string {
-    console.log('random function ' + arg);
-
-    return 'hello';
-}
-
