28e913c5ed618c9c6bf652bf15844ceaebb20032
Jenek Fedorow <efedorov@bivgroup.ru>
2023-06-21 17:25:01 +0300

    added button component

--- src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -3,6 +3,7 @@ import cls from './ThemeSwitcher.module.scss';
 import {Theme, useTheme} from "app/providers/ThemeProvider";
 import MoonIcon from 'shared/assets/icons/moon.svg';
 import SunIcon from 'shared/assets/icons/sun.svg';
+import {Button, ThemeButton} from "shared/ui/Button/Button";
 
 interface ThemeSwitcherProps {
     className?: string;
@@ -12,11 +13,12 @@ export const ThemeSwitcher = ({className}: ThemeSwitcherProps) => {
     const {theme, toggleTheme} = useTheme();
 
     return (
-        <button
+        <Button
             className={classNames(cls.ThemeSwitcher, {}, [className])}
             onClick={toggleTheme}
+            theme={ThemeButton.CLEAR}
         >
             { theme === Theme.DEFAULT ? <SunIcon /> : <MoonIcon />}
-        </button>
+        </Button>
     )
 }
