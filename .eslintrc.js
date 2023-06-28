ff83b8cd2a1555c4047c122b773d48c1d558c2ad
Jenek Fedorow <efedorov@bivgroup.ru>
2023-06-28 11:16:39 +0300

    linter fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -38,7 +38,7 @@ module.exports = {
         'import/extensions': 'off',
         'import/no-extraneous-dependencies': 'off',
         'no-underscore-dangle': 'off',
-        'i18next/no-literal-string': ['error', { markupOnly: true, ignoreAttribure: ['data-testid'] }],
+        'i18next/no-literal-string': ['error', { markupOnly: true, ignoreAttribute: ['data-testid', 'to'] }],
         'max-len': ['error', { ignoreComments: true, code: 115 }],
     },
     globals: {
@@ -49,7 +49,7 @@ module.exports = {
             files: ['**/src/**/*.test.{ts,tsx}'],
             rules: {
                 'i18next/no-literal-string': 'off',
-            }
-        }
-    ]
+            },
+        },
+    ],
 };
