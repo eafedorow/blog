f0975cc2d89b161db775414b6e559626d6097665
Jenek Fedorow <efedorov@bivgroup.ru>
2023-07-05 16:52:37 +0300

    added links in sidebar

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
-import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
 import { useTranslation } from 'react-i18next';
 import cls from './Navbar.module.scss';
 
@@ -13,22 +12,7 @@ const Navbar = ({ className }: NavbarProps) => {
 
     return (
         <div className={classNames(cls.Navbar, {}, [className])}>
-            <div className={cls.links}>
-                <AppLink
-                    className={cls.link}
-                    to="/"
-                    theme={AppLinkTheme.SECONDARY}
-                >
-                    {t('Главная')}
-                </AppLink>
-                <AppLink
-                    className={cls.link}
-                    to="/about"
-                    theme={AppLinkTheme.SECONDARY}
-                >
-                    {t('О сайте')}
-                </AppLink>
-            </div>
+            <div className={cls.links} />
         </div>
     );
 };
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -1,9 +1,12 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { Button, ButtonSize, ButtonTheme } from 'shared/ui/Button/Button';
 import { ThemeSwitcher } from 'widgets/ThemeSwitcher';
 import { LangSwitcher } from 'widgets/LangSwitcher/ui/LangSwitcher';
 import { useTranslation } from 'react-i18next';
+import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
+import MainIcon from 'shared/assets/icons/home.svg';
+import AboutIcon from 'shared/assets/icons/calendar-info.svg';
 import cls from './Sidebar.module.scss';
 
 interface SidebarProps {
@@ -23,6 +26,25 @@ export const Sidebar = ({ className }: SidebarProps) => {
             data-testid="sidebar"
             className={classNames(cls.Sidebar, { [cls.collapsed]: isCollapsed }, [className])}
         >
+            <div className={cls.linksList}>
+                <AppLink
+                    className={cls.link}
+                    to="/"
+                    theme={AppLinkTheme.SECONDARY}
+                >
+                    <MainIcon className={cls.linkIcon} />
+                    {t('Главная')}
+                </AppLink>
+                <AppLink
+                    className={cls.link}
+                    to="/about"
+                    theme={AppLinkTheme.SECONDARY}
+                >
+                    <AboutIcon className={cls.linkIcon} />
+                    {t('О сайте')}
+                </AppLink>
+            </div>
+
             <Button
                 className={cls.collapseBtn}
                 theme={ButtonTheme.BACKGROUND}
@@ -33,6 +55,7 @@ export const Sidebar = ({ className }: SidebarProps) => {
             >
                 {isCollapsed ? '>' : '<'}
             </Button>
+
             <div className={cls.switchers}>
                 <ThemeSwitcher />
                 <LangSwitcher
