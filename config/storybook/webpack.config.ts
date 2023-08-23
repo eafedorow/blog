35d1a3027a9480731d67d1eea71ce525d4adc0b1
eafed <eafedorow@yandex.ru>
2023-08-23 19:30:17 +0300

    refactored webpack

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -13,14 +13,12 @@ export default ({ config }: {config: webpack.Configuration}) => {
     config!.resolve!.modules!.push(paths.src);
     config!.resolve!.extensions!.push('.ts', '.tsx');
 
-    // eslint-disable-next-line no-param-reassign
-    // @ts-ignore
-    config!.module!.rules = config!.module!.rules!.map((rule: RuleSetRule) => {
-        if (/svg/.test(rule.test as string)) {
-            return { ...rule, exclude: /\.svg$/i };
-        }
-        return rule;
-    });
+    const rules = config.module!.rules as RuleSetRule[];
+    config.module!.rules = rules.map((rule) => (
+        /svg/.test(rule.test as string)
+            ? { ...rule, exclude: /\.svg$/i }
+            : rule
+    ));
 
     config!.module!.rules.push({
         test: /\.svg$/,
